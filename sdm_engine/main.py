e59e9f468a01d91a609f4ee542e693b174f1e8cb
maxiee <maxieewong@gmail.com>
2022-10-09 00:35:56 +0800

    get_calendar_day

--- sdm_engine/main.py
+++ sdm_engine/main.py
@@ -1,5 +1,8 @@
 import uvicorn
+from datetime import date
 from fastapi import FastAPI
+
+from sdm_engine.mongo import init_mongo
 
 app = FastAPI()
 
@@ -7,7 +10,14 @@ app = FastAPI()
 def hello():
     return "Hello, Stock Data Manager!"
 
+
+# Calendar related APIs
+@app.get('/calendar/{day}')
+def get_calendar_day(day: date):
+    return day
+
 def start():
+    init_mongo()
     uvicorn.run(
         'sdm_engine.main:app', 
         host='127.0.0.1',
--- sdm_engine/mongo.py
+++ sdm_engine/mongo.py
@@ -3,4 +3,5 @@ from pymongo import MongoClient
 client = None
 
 def init_mongo():
+    global client
     client = MongoClient()
