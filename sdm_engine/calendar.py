8e7c5a3f5b4b5a9b2465150685a6cb0f08706255
maxiee <maxieewong@gmail.com>
2022-10-09 10:15:50 +0800

    add comment

--- sdm_engine/calendar.py
+++ sdm_engine/calendar.py
@@ -1,11 +1,14 @@
 from datetime import date
-from os import times_result
+
 from sdm_engine import mongo
 from sdm_engine.constants import Exchange
 from sdm_engine.utils import time_utils
 
 
 def set_calendar_day(day: date, exchange: Exchange, working: bool):
+    """
+    Set the working date of the stock exchange
+    """
     mongo.collection_calendar.update_one(
         {"day": time_utils.date_to_datetime(day), "exchange": exchange},
         {"$set": {"working": working}},
